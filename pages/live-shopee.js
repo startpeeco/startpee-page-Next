debaf8416d7704129b648f3df146a97a696b299f
startpeeco <134424987+startpeeco@users.noreply.github.com>
2023-12-19 15:55:14 -0300

    Commit from TeleportHQ

--- pages/live-shopee.js
+++ pages/live-shopee.js
@@ -9,6 +9,14 @@ const LiveShopee = (props) => {
           <title>live-shopee - startpee page</title>
           <meta property="og:title" content="live-shopee - startpee page" />
         </Head>
+        <img
+          src="/shopee-lives-contribuem-com-vendas-e-engajamento-de-consumidores-e-vendedores-no-brasil-1500w.png"
+          alt="image"
+          className="live-shopee-image"
+        />
+        <div className="live-shopee-get-started">
+          <span className="live-shopee-text">Quero ativar</span>
+        </div>
       </div>
       <style jsx>
         {`
@@ -19,6 +27,46 @@ const LiveShopee = (props) => {
             min-height: 100vh;
             align-items: center;
             flex-direction: column;
+            justify-content: center;
+          }
+          .live-shopee-image {
+            width: 100%;
+            height: 434px;
+            object-fit: cover;
+          }
+          .live-shopee-get-started {
+            color: white;
+            cursor: pointer;
+            display: flex;
+            margin-top: 40px;
+            transition: 0.3s;
+            align-items: center;
+            padding-top: var(--dl-space-space-unit);
+            padding-left: var(--dl-space-space-twounits);
+            border-radius: 56px;
+            padding-right: var(--dl-space-space-twounits);
+            flex-direction: column;
+            padding-bottom: var(--dl-space-space-unit);
+            background-color: #000000;
+          }
+          .live-shopee-get-started:hover {
+            color: black;
+            background-color: #ffffff;
+          }
+          .live-shopee-text {
+            font-size: 18px;
+            font-style: normal;
+            font-weight: 600;
+          }
+          @media (max-width: 767px) {
+            .live-shopee-get-started {
+              margin-top: var(--dl-space-space-twounits);
+            }
+          }
+          @media (max-width: 479px) {
+            .live-shopee-get-started {
+              width: 100%;
+            }
           }
         `}
       </style>

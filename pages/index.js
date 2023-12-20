1e13ebb90c13b1d0a1e4159ab59ad65dc41c7b25
startpeeco <134424987+startpeeco@users.noreply.github.com>
2023-12-20 15:45:56 -0300

    Commit from TeleportHQ

--- pages/index.js
+++ pages/index.js
@@ -2261,7 +2261,7 @@ Here's what the above is doing:
               font-weight: 700;
             }
             .home-text14 {
-              color: rgb(238, 233, 254);
+              color: var(--dl-color-gray-white);
               font-size: 20px;
               align-self: center;
               line-height: 24px;
